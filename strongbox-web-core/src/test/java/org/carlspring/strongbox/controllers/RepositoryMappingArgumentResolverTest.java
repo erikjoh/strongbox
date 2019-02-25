0710f46e9382517c22de4912af8d9d4be45e2669
Nicklas Hersén <nhersen@kth.se>
2019-02-25 20:45:57 +0100

    TST: Add BeforeEach annotation in RepositoryMappingArgumentResolverTest.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/RepositoryMappingArgumentResolverTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/RepositoryMappingArgumentResolverTest.java
@@ -5,15 +5,26 @@ import java.lang.annotation.RetentionPolicy;
 
 import org.carlspring.strongbox.controllers.RepositoryMappingArgumentResolver;
 import org.springframework.core.MethodParameter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
+
+/**
+ * @author Nicklas Hersén
+ */
 
 /**
  * Declare a new annotation returned in mock object.
@@ -31,6 +42,14 @@ class TestDemoClass
 
 public class RepositoryMappingArgumentResolverTest
 {
+    private RepositoryMappingArgumentResolver resolver;
+
+    @BeforeEach
+    private void setup()
+    {
+	resolver = new RepositoryMappingArgumentResolver();
+    }
+
     /**
      * Ensure that supportsParameter returns false when getParameterAnnotation
      * returns null.
@@ -38,9 +57,6 @@ public class RepositoryMappingArgumentResolverTest
     @Test
     public void testSupportsParameterNull()
     {
-        RepositoryMappingArgumentResolver resolver =
-            new RepositoryMappingArgumentResolver();
-
         MethodParameter mp = mock(MethodParameter.class);
         when(mp.getParameterAnnotation(any(java.lang.Class.class)))
             .thenReturn(null);
@@ -56,9 +72,6 @@ public class RepositoryMappingArgumentResolverTest
     @Test
     public void testSupportsParameterNotNull()
     {
-        RepositoryMappingArgumentResolver resolver =
-            new RepositoryMappingArgumentResolver();
-
         TestDemoClass demoClass = new TestDemoClass();
         Class c = demoClass.getClass();
 
